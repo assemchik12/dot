e9d9717f5c8e8ddc60d979b4545d8db963398ec4
Assemchik1 <118967701+Assemchik1@users.noreply.github.com>
2022-11-28 23:41:08 +0500

    5

---
+++ 5/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите имя пользователя ");
+string username = Console.ReadLine();
+
+if(username == "Маша ");
+ Console.WriteLine ("Ура, это же Маша ");   
+
+
+ Console.Write ("Привет, ");    
+Console.WriteLine (username );
+
